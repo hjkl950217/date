5a9253bf8e569495e150771a73c30a0cf095c9b3
长空X <hjkl950217@163.com>
2017-12-23 17:05:46 +0800

    Add files via upload

---
+++ SellerInfo_Lynn.cs
@@ -0,0 +1,30 @@
+﻿using NE.MPS.Framework.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NE.MPS.Domain.Model.Seller
+{
+    /// <summary>
+    /// 卖家信息-普通实体
+    /// </summary>
+   public class SellerInfo_Lynn
+    {
+        /// <summary>
+        /// 卖家ID
+        /// </summary>
+        public string SellerID { get; set; }
+        /// <summary>
+        /// 卖家存储的姓名
+        /// </summary>
+        public string SellerName { get; set; }
+
+        /// <summary>
+        ///  卖家状态
+        /// </summary>
+        public eSellerStatus Status { get; set; }
+
+
+    }
+}
